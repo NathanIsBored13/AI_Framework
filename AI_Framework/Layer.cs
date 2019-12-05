55a7c38f25871b8cc56b00fabe4be8444dbb2dec
nathan smith <nathan.smith@canterburians.org.uk>
2019-12-05 22:15:57 +0000

    Add project files.

---
+++ AI_Framework/Layer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AI_Framework
+{
+    class Layer
+    {
+        private Layer parent, child;
+        private int node_width;
+        private double[] biases;
+        private double[,] weights;
+        private Random random;
+        public Layer(Layer parent, int[] structure, int index)
+        {
+            node_width = structure[index];
+            this.parent = parent;
+            if (index != structure.Length - 1) child = new Layer(this, structure, index + 1);
+            else child = null;
+            biases = new double[structure[index]];
+            weights = new double[structure[index], structure[index - 1]];
+            random = new Random();
+            for (int x = 0; x < structure[index]; x++)
+            {
+                biases[x] = random.NextDouble() * 10d - 5d;
+                for (int y = 0; y < structure[index - 1]; y++)
+                {
+                    weights[x, y] = random.NextDouble() * 10d - 5d;
+                }
+            }
+        }
+        public double[] Propegate_Down(double[] activation)
+        {
+            double[] buffer = new double[node_width];
+            for (int x = 0; x < node_width; x++)
+            {
+                for (int y = 0; y < activation.Length; y++)
+                {
+                    buffer[x] += activation[y] * weights[x, y];
+                }
+                buffer[x] = 1d / (1d + Math.Exp(biases[x] - buffer[x]));
+            }
+            if (child != null) buffer = child.Propegate_Down(buffer);
+            return buffer;
+        }
+    }
+}
---
+++ AI_Framework/Neural_Network.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AI_Framework
+{
+    class Neural_Network
+    {
+        public int[] structure { get; }
+        private Layer input;
+        public Neural_Network(int[] structure)
+        {
+            this.structure = structure;
+            input = new Layer(null, structure, 1);
+        }
+        public double[] Compute_Network(double[] input) => this.input.Propegate_Down(input);
+    }
+}
