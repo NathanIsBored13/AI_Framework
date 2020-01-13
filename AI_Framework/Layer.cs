fdabae14bf1cdd2864344413cc2c95f0d6517a35
nathan smith <nathan.smith@canterburians.org.uk>
2020-01-13 22:32:57 +0000

    Optimised file reading and writing

--- AI_Framework/Layer.cs
+++ AI_Framework/Layer.cs
@@ -1,47 +1,59 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace AI_Framework
 {
     class Layer
     {
-        private Layer parent, child;
-        private int node_width;
-        private double[] biases;
         private double[,] weights;
-        private Random random;
-        public Layer(Layer parent, int[] structure, int index)
+        private double[] biases;
+        private int depth { get; }
+        private Layer child;
+        public Layer(int[] structure, int depth)
         {
-            node_width = structure[index];
-            this.parent = parent;
-            if (index != structure.Length - 1) child = new Layer(this, structure, index + 1);
+            this.depth = depth;
+            Console.WriteLine("layer {0} created", depth);
+            if (depth != structure.Length - 1) child = new Layer(structure, depth + 1);
             else child = null;
-            biases = new double[structure[index]];
-            weights = new double[structure[index], structure[index - 1]];
-            random = new Random();
-            for (int x = 0; x < structure[index]; x++)
+            biases = new double[structure[depth]];
+            weights = new double[structure[depth], structure[depth - 1]];
+            Random random = new Random();
+            for (int x = 0; x < structure[depth]; x++)
             {
                 biases[x] = random.NextDouble() * 10d - 5d;
-                for (int y = 0; y < structure[index - 1]; y++)
+                for (int y = 0; y < structure[depth - 1]; y++)
                 {
                     weights[x, y] = random.NextDouble() * 10d - 5d;
                 }
             }
         }
-        public double[] Propegate_Down(double[] activation)
+        public Layer(File_Maneger file_maneger, int depth)
         {
-            double[] buffer = new double[node_width];
-            for (int x = 0; x < node_width; x++)
+            this.depth = depth;
+            LayerInfo buffer = file_maneger.ReadNext();
+            weights = buffer.weights;
+            biases = buffer.biases;
+            Console.WriteLine("layer {0} loaded", depth);
+            if (file_maneger.IsMore()) child = new Layer(file_maneger, depth + 1);
+        }
+        public double[] PropegateDown(double[] activation)
+        {
+            double[] buffer = new double[weights.GetLength(0)];
+            for (int x = 0; x < weights.GetLength(0); x++)
             {
-                for (int y = 0; y < activation.Length; y++)
+                for (int y = 0; y < weights.GetLength(1); y++)
                 {
                     buffer[x] += activation[y] * weights[x, y];
                 }
                 buffer[x] = 1d / (1d + Math.Exp(biases[x] - buffer[x]));
             }
-            if (child != null) buffer = child.Propegate_Down(buffer);
+            if (child != null) buffer = child.PropegateDown(buffer);
             return buffer;
+        }
+        public void Export(File_Maneger file_Manager)
+        {
+            file_Manager.Export(depth, weights, biases);
+            Console.WriteLine("Exported layer {0}", depth);
+            if (child != null) child.Export(file_Manager);
         }
     }
 }
--- AI_Framework/Neural_Network.cs
+++ AI_Framework/Neural_Network.cs
@@ -4,15 +4,46 @@ using System.Text;
 
 namespace AI_Framework
 {
+    struct LayerInfo
+    {
+        public double[,] weights;
+        public double[] biases;
+    }
     class Neural_Network
     {
         public int[] structure { get; }
+        private double[] buffer;
         private Layer input;
         public Neural_Network(int[] structure)
         {
             this.structure = structure;
-            input = new Layer(null, structure, 1);
+            input = new Layer(structure, 1);
+            buffer = new double[structure[structure.Length - 1]];
         }
-        public double[] Compute_Network(double[] input) => this.input.Propegate_Down(input);
+        public Neural_Network(string location)
+        {
+            File_Maneger file_maneger = new File_Maneger(location);
+            input = new Layer(file_maneger, 1);
+            structure = file_maneger.GetStructure();
+        }
+        public double[] ComputeNetwork(double[] input)
+        {
+            buffer = this.input.PropegateDown(input);
+            return buffer;
+        }
+        public double ComputeCost(int correct)
+        {
+            double ret = 0;
+            for (int i = 0; i < buffer.Length; i++) ret += Math.Pow(buffer[i] - (i == correct ? 1 : 0), 2);
+            return ret;
+        }
+        public void Export(string location)
+        {
+            File_Maneger file_maneger = new File_Maneger(location);
+            file_maneger.Erase();
+            Console.WriteLine("Erased folder at {0}", location);
+            input.Export(file_maneger);
+            Console.WriteLine("Compleated export");
+        }
     }
 }
